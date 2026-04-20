a21de8d5f29b7bb1439d94658af4e7a47b029e5d
Nikhil Mani <nikhilrmani@gmail.com>
2026-04-19 23:00:33 -0400

    write eye to hand code, simplify eye in hand

--- vs.py
+++ vs.py
@@ -2,9 +2,12 @@ import numpy as np
 
 def calculate_matrix(u: int, v: int, lmbda: float, z: float):
     # find the interaction matrix for one feature
-    return np.array([lmbda/z, 0, -u/z, -(u*v/z), (lmbda**2 + u**2)/lmbda, -v], 
-                    [0, lmbda/z, -v/z, -(lmbda**2 - v**2)/lmbda, (u*v)/lmbda, u])
+    return np.array([
+        [lmbda/z, 0, -u/z, -(u*v/lmbda), (lmbda**2 + u**2)/lmbda, -v], 
+        [0, lmbda/z, -v/z, -(lmbda**2 + v**2)/lmbda, (u*v)/lmbda, u]
+                    ])
 
+# returns L_e (interaction matrix, 2k x 6)
 def find_interaction(points: list, f, z):
     # points is list of k (u,v) pairs
     # f is focal len
@@ -13,24 +16,23 @@ def find_interaction(points: list, f, z):
     for i, (u, v) in enumerate(points):
         L_e.append(calculate_matrix(u, v, f, z[i]))
 
-    return np.vstack([L_e])
-# this function will solve for dr given du, dv, and an interaction matrix
-def find_velocity(df: list, J, m):
-    # check the dimensionality of k and m
-    # k = rows / 2
-    k = J.shape[0] / 2
-    Jinv = np.linalg.inv(J)
-    # J inverse exists!
-    if(k==m):
-        r = Jinv @ df
-    if(k > m):
-        # left psuedo inverse
-        psuedoinv = np.linalg.inv(J.T @ J) @ J.T
-        r = psuedoinv @ df
-    if(k < m):
-        # right psuedo inverse
-        psuedoinv = J.T @ np.linalg.inv(J.T @ J) 
-        r = psuedoinv @ df
-    return r
+    return np.vstack(L_e)
+
+# eye in hand control
+# outputs joint velocities (6 x 1) given L_e (interaction matrix, 2k x 6), e (vector of errors in each feature, 2k x 1)
+# k (list of PID proportionality constants), and v (list of past velocities)
+# TODO: finish PID control
+def eih_find_velocity(e: list, L_e, k: list, v: list):
+    # np pinv function automatically calculates psuedo inverse
+    psuedoinv = np.linalg.pinv(L_e)
+    return k[0] * psuedoinv @ e
+
+# eye to hand control
+# same as above, but also requires robot jacobian and V, translation matrix
+# assume at tool ce
+def eth_find_velocity(df: list, L_e, J, k: list, e, V):
+    psuedoinv = np.linalg.pinv(L_e @ V @ J)
+    return k[0] * psuedoinv @ e
+
 
 
